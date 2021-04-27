b608b1a6e04cf23d4dff2dd9711f521b4eaab32a
brutalchrist <brutalchrist@gmail.com>
2021-04-27 18:47:41 -0400

    add fields

--- src/schemas/order.schema.ts
+++ src/schemas/order.schema.ts
@@ -1,12 +1,27 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 
 export type OrderDocument = Order & Document;
 
 @Schema()
 export class Order {
-  @Prop()
-  date: Date;
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' })
+  user: MongooseSchema.Types.ObjectId;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Exercise' })
+  exercise: MongooseSchema.Types.ObjectId ;
+
+  @Prop(raw({
+    type: { type: String },
+    coordinates: [Number],
+  }))
+  location: Record<string, any>;
+
+  @Prop([raw({
+    name: { type: String },
+    amount: Number,
+  })])
+  products: [Record<string, any>];
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
