184bd8ba248f43999ebba6e265d0bffa84fc1363
brutalchrist <brutalchrist@gmail.com>
2021-04-27 00:26:19 -0400

    add config module for root

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrderServiceModule } from 'src/services/order/order-service.module';
 import { OrderController } from './order/order.controller';
 
 @Module({
-  imports: [OrderServiceModule],
+  imports: [ConfigModule.forRoot(), OrderServiceModule],
   controllers: [OrderController],
   providers: [],
 })
