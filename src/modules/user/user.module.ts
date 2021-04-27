c5a84ed60813179e884449861bc6f592271f8e83
brutalchrist <brutalchrist@gmail.com>
2021-04-27 17:45:31 -0400

    fix schema mongoose

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Order, OrderSchema } from 'src/schemas/order.schema';
+import { User, UserSchema } from 'src/schemas/user.schema';
 import { UserServiceModule } from 'src/services/user/user-service.module';
 import { UserController } from './user.controller';
 
 @Module({
   imports: [
     UserServiceModule,
-    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
   ],
   controllers: [UserController],
 })
